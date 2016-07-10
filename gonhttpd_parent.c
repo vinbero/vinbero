bcc37a90384e331ddcc066bb03e013ee639c3cf7
Byeonggon Lee <gonny952@gmail.com>
2016-07-10 21:50:25 +0900

    Socket test

--- gonhttpd_parent.c
+++ gonhttpd_parent.c
@@ -1,7 +1,11 @@
+#include <arpa/inet.h>
 #include <libgonc/gonc_list.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include "gonhttpd_parent.h"
@@ -27,13 +31,40 @@ void gonhttpd_parent_sigchld_handler(int signal_name)
 
 void gonhttpd_parent_start(struct gonhttpd_socket_list* child_socket_list)
 {
+    int server_socket, client_socket;
+    struct sockaddr_in server_address, client_address;
+    socklen_t client_address_length;
+    memset(server_address.sin_zero, 0, sizeof(server_address.sin_zero));
+    server_address.sin_family = AF_INET;
+    inet_aton("0.0.0.0", &server_address.sin_addr);
+    server_address.sin_port = htons(8080);
+    if((server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
+    {
+        fprintf(stderr, "socket error\n");
+        exit(1);
+    }
+    bool is_socket_reusable = true;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (void*)is_socket_reusable, sizeof(is_socket_reusable));
+    if(bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1)
+    {
+        fprintf(stderr, "bind error\n");
+        exit(1);
+    }
+    if(listen(server_socket, 10) == -1)
+    {
+        fprintf(stderr, "listen error\n");
+        exit(1);
+    }
+
     while(true)
     {
-        sleep(1);
-        GONC_LIST_FOR_EACH(child_socket_list, struct gonhttpd_socket_list_element, element)
-        {
-            printf("PARENT pid: %u, child socket: %d \n", getpid(), element->socket);
-        }
+        client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_length);
+        printf("client_address_length: %u\n", client_address_length);
+        if(client_socket == -1)
+            fprintf(stderr, "accept error\n");
+        write(client_socket, "HTTP/1.1 200 OK\r\nServer: gonhttpd\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n", sizeof("HTTP/1.1 200 OK\r\nServer: gonhttpd\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n"));
+        close(client_socket);
+
     }
 }
 
