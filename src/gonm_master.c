7ffa363cc3936ed19e0b30325b907fa000e161f5
Byeonggon Lee <gonny952@gmail.com>
2016-07-24 07:40:36 +0900

    gonm_module_destroy

--- src/gonm_master.c
+++ src/gonm_master.c
@@ -41,10 +41,16 @@ void gonm_master_start(struct gonm_master_args* master_args)
     void* dl_handle;
     if((dl_handle = dlopen(GONC_LIST_HEAD(master_args->module_args_list)->module_path.chars, RTLD_LAZY)) == NULL)
         err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
+
     int (*gonm_module_init)(struct gonm_module_args*, struct gonm_module_list*);
     if((gonm_module_init = dlsym(dl_handle, "gonm_module_init")) == NULL)
         err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
+
     if((master_args->worker_args->gonm_module_service = dlsym(dl_handle, "gonm_module_service")) == NULL)
+        err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
+
+    int (*gonm_module_destroy)(struct gonm_module*);
+    if((gonm_module_destroy = dlsym(dl_handle, "gonm_module_destroy")) == NULL)
         err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
 
     struct gonm_module_list* module_list = malloc(sizeof(struct gonm_module_list));
@@ -53,7 +59,14 @@ void gonm_master_start(struct gonm_master_args* master_args)
     if(gonm_module_init(GONC_LIST_HEAD(master_args->module_args_list), module_list) == -1)
         err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
 
-    //TODO: destroy module_args_list
+    GONC_LIST_FOR_EACH(master_args->module_args_list, struct gonm_module_args, module_args)
+    {
+        GONC_LIST_FOR_EACH(module_args, struct gonm_module_arg, module_arg)
+            free(module_arg);
+        free(module_args->module_path.chars);
+        free(module_args);
+    }
+    free(master_args->module_args_list);
     master_args->worker_args->module = GONC_LIST_HEAD(module_list);
 
     for(size_t index = 0; index != master_args->worker_count; ++index)
@@ -64,7 +77,9 @@ void gonm_master_start(struct gonm_master_args* master_args)
 
     for(size_t index = 0; index != master_args->worker_count; ++index)
         pthread_join(threads[index], NULL);
-    //TODO: destroy module_list
+    if(gonm_module_destroy(GONC_LIST_HEAD(module_list)) == -1)
+        warn("%s: %u", __FILE__, __LINE__);
+    free(module_list);
     dlclose(dl_handle);
     pthread_mutex_destroy(master_args->worker_args->server_socket_mutex);
     free(master_args->worker_args->server_socket_mutex);
--- src/test_module.c
+++ src/test_module.c
@@ -16,3 +16,8 @@ int gonm_module_service(struct gonm_module* module, int client_socket)
     write(client_socket, "HTTP/1.1 200 OK\r\nServer: gonm\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n", sizeof("HTTP/1.1 200 OK\r\nServer: gonm\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n"));
     return 0;
 }
+
+int gonm_module_destroy(struct gonm_module* module)
+{
+    return 0;
+}
