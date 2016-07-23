2c0da8d12c509f10997768c0cc5ae1d8138982e8
Byeonggon Lee <gonny952@gmail.com>
2016-07-24 03:24:30 +0900

    Modify gonm_worker.c

--- src/gonm_worker.c
+++ src/gonm_worker.c
@@ -18,7 +18,11 @@ void* gonm_worker_start(void* worker_args)
             continue;
         }
         pthread_mutex_unlock(((struct gonm_worker_args*)worker_args)->server_socket_mutex);
-        write(client_socket, "HTTP/1.1 200 OK\r\nServer: gonm\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n", sizeof("HTTP/1.1 200 OK\r\nServer: gonm\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n"));
+
+        write(client_socket, "HTTP/1.1 200 OK\r\nServer: gonm\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n", sizeof("HTTP/1.1 200 OK\r\nServer: gonm\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n"));        
+//        struct gonm_module_object_list* module_object_list = gonm_module_init(((struct gonm_worker_args*)worker_args)->context, ((struct gonm_worker_args*)worker_args)->module_args_list);
+//        gonm_module_service(((struct gonm_worker_args*)worker_args)->context, GONC_LIST_HEAD(module_object_list), client_socket);
+//        GONM_MODULE_OBJECT_LIST_DESTROY(module_object_list);
         
         if(close(client_socket) == -1)
             warn("%s:%d", __FILE__, __LINE__);
