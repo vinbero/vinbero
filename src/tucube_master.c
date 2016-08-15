c54285b138368981e89b418b958b161d8d1722a5
Byeonggon Lee <gonny952@gmail.com>
2016-08-16 01:15:37 +0900

    Appends tucube-worker-count argument to all modules

--- src/tucube_master.c
+++ src/tucube_master.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <setjmp.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
@@ -91,9 +92,30 @@ void tucube_master_init_modules(struct tucube_master_args* master_args)
     if((master_args->tucube_module_destroy = dlsym(master_args->dl_handle, "tucube_module_destroy")) == NULL)
         errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_module_destroy()", __FILE__, __LINE__);
 
+    size_t worker_count = master_args->worker_count;
+    size_t worker_count_length = 0;
+    while(worker_count != 0)
+    {
+        worker_count /= 10;
+        ++worker_count_length;
+    }
+
+    GONC_LIST_FOR_EACH(master_args->module_args_list, struct tucube_module_args, module_args)
+    {
+        struct tucube_module_arg* module_arg = malloc(sizeof(struct tucube_module_arg));
+        GONC_LIST_ELEMENT_INIT(module_arg);
+        GONC_STRING_SET(&module_arg->name, strdup("tucube-worker-count"));
+        module_arg->value.chars = malloc(worker_count_length + 1);
+        module_arg->value.chars[worker_count_length] = '\0';
+        module_arg->value.length = worker_count_length;
+
+        if(snprintf(module_arg->value.chars, worker_count_length + 1, "%d", master_args->worker_count) < 0)
+            errx(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
+        GONC_LIST_APPEND(module_args, module_arg);
+    }
+
     master_args->module_list = malloc(sizeof(struct tucube_module_list));
     GONC_LIST_INIT(master_args->module_list);
-
     if(master_args->tucube_module_init(GONC_LIST_HEAD(master_args->module_args_list), master_args->module_list) == -1)
         errx(EXIT_FAILURE, "%s: %u: tucube_module_init() failed", __FILE__, __LINE__);
 
@@ -124,7 +146,11 @@ void tucube_master_start(struct tucube_master_args* master_args)
             if(pthread_create(worker_threads + index, &worker_thread_attr, tucube_worker_start, master_args->worker_args) != 0)
                 err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
         }
-        pause();
+        for(size_t index = 0; index != master_args->worker_count; ++index)
+        {
+            void* return_value;
+            pthread_join(worker_threads[index], &return_value);
+        }
     }
     free(jump_buffer);
     pthread_key_delete(tucube_master_tlkey);
