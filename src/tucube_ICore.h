9de4443d774a674fa480455f0e82fffee6383e74
Byeonggon Lee <gonny952@gmail.com>
2017-06-07 02:54:04 +0900

    Fix typo

--- src/tucube_ICore.h
+++ src/tucube_ICore.h
@@ -5,7 +5,7 @@
 #include "tucube_Module.h"
 
 #define TUCUBE_ICORE_FUNCTIONS \
-int tucube_ICore_service(struct tucube_Module* module, int* clientSocket, pthread_mutex_t* clientSocketMutex)
+int tucube_ICore_service(struct tucube_Module* module, int* serverSocket, pthread_mutex_t* serverSocketMutex)
 
 #define TUCUBE_ICORE_FUNCTION_POINTERS \
 int (*tucube_ICore_service)(struct tucube_Module*, int*, pthread_mutex_t*)
