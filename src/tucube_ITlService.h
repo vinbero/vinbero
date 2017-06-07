b7e35136769158cac935673430c66701c0b06bba
Byeonggon Lee <gonny952@gmail.com>
2017-06-07 14:51:27 -0400

    This naming will be better

--- src/tucube_ITlService.h
+++ src/tucube_ITlService.h
@@ -1,16 +1,16 @@
-#ifndef _TUCUBE_ITL_SERVICE_H
-#define _TUCUBE_ITL_SERVICE_H
+#ifndef _TUCUBE_ITLSERVICE_H
+#define _TUCUBE_ITLSERVICE_H
 
 #include <pthread.h>
 #include "tucube_Module.h"
 
-#define TUCUBE_ITL_SERVICE_FUNCTIONS \
+#define TUCUBE_ITLSERVICE_FUNCTIONS \
 int tucube_ITlService_call(struct tucube_Module* module, void* args[])
 
-#define TUCUBE_ITL_SERVICE_FUNCTION_POINTERS \
+#define TUCUBE_ITLSERVICE_FUNCTION_POINTERS \
 int (*tucube_ITlService_call)(struct tucube_Module*, void*[])
 
-#define TUCUBE_ITL_SERVICE_DLSYM(module, modulePointerType) \
+#define TUCUBE_ITLSERVICE_DLSYM(module, modulePointerType) \
 TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_ITlService_call)
 
 #endif
