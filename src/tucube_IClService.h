b88edcfb39635eaf8f876c47b1cfe4c1000a61a8
Byeonggon Lee <gonny952@gmail.com>
2017-06-07 14:51:27 -0400

    This naming will be better

--- src/tucube_IClService.h
+++ src/tucube_IClService.h
@@ -1,19 +1,19 @@
-#ifndef _TUCUBE_ICL_SERVICE_H
-#define _TUCUBE_ICL_SERVICE_H
+#ifndef _TUCUBE_ICLSERVICE_H
+#define _TUCUBE_ICLSERVICE_H
 
 #include "tucube_Module.h"
 
-#define TUCUBE_ICL_SERVICE_FUNCTIONS                                                                           \
+#define TUCUBE_ICLSERVICE_FUNCTIONS                                                                           \
 int tucube_IClService_Init(struct tucube_Module* module, struct tucube_ClData_List* clDataList, void* args[]); \
 int tucube_IClService_call(struct tucube_Module* module, struct tucube_ClData* clData, void* args[]);       \
 int tucube_IClService_destroy(struct tucube_Module* module, struct tucube_ClData* clData)
 
-#define TUCUBE_ICL_SERVICE_FUNCTION_POINTERS                                                    \
+#define TUCUBE_ICLSERVICE_FUNCTION_POINTERS                                                    \
 int (*tucube_IClService_init)(struct tucube_Module*, struct tucube_ClData_List*, void*[]);      \
 int (*tucube_IClService_call)(struct tucube_Module*, struct tucube_ClData*, void*[]); \
 int (*tucube_IClService_destroy)(struct tucube_Module*, struct tucube_ClData*)
 
-#define TUCUBE_ICL_SERVICE_DLSYM(module, modulePointerType)                \
+#define TUCUBE_ICLSERVICE_DLSYM(module, modulePointerType)                \
 TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IClService_init);    \
 TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IClService_call); \
 TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IClService_destroy)
