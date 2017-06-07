15a39ab5cfe0edc40ef19e605f7706b1a0b581e7
Byeonggon Lee <gonny952@gmail.com>
2017-06-07 08:11:15 -0400

    Fixed stupid typos & serverSocketMutex is useless

--- src/tucube_IClientLocal.h
+++ src/tucube_IClientLocal.h
@@ -8,10 +8,10 @@ int tucube_IClientLocal_Init(struct tucube_Module* module, struct tucube_ClData_
 int tucube_IClientLocal_service(struct tucube_Module* module, struct tucube_ClData* clData, void* args[]);       \
 int tucube_IClientLocal_destroy(struct tucube_Module* module, struct tucube_ClData* clData)
 
-#define TUCUBE_ICLIENT_LOCAL_FUNCTION_POINTERS                                                   \
-int (*tucube_tcp_epoll_Module_init)(struct tucube_Module*, struct tucube_ClData_List*, void*[]); \
-int (*tucube_tcp_epoll_Module_service)(struct tucube_Module*, struct tucube_ClData*, void*[]);   \
-int (*tucube_tcp_epoll_Module_destroy)(struct tucube_Module*, struct tucube_ClData*)
+#define TUCUBE_ICLIENT_LOCAL_FUNCTION_POINTERS                                                    \
+int (*tucube_IClientLocal_init)(struct tucube_Module*, struct tucube_ClData_List*, void*[]);      \
+int (*tucube_IClientLocal_service)(struct tucube_Module*, struct tucube_ClData*, void*[]); \
+int (*tucube_IClientLocal_destroy)(struct tucube_Module*, struct tucube_ClData*)
 
 #define TUCUBE_ICLIENT_LOCAL_DLSYM(module, modulePointerType)                \
 TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IClientLocal_init);    \
