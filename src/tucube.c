bb5e07dc67405b3e95a1c042e141aeda5db71e59
Byeonggon Lee <gonny952@gmail.com>
2017-07-02 04:22:11 +0000

    libgon_c -> libgenc

--- src/tucube.c
+++ src/tucube.c
@@ -1,5 +1,5 @@
 #include <jansson.h>
-#include <libgon_c/gon_c_list.h>
+#include <libgenc/genc_list.h>
 #include "config.h"
 #include "tucube_Core.h"
 #include "tucube_Options.h"
@@ -9,11 +9,11 @@ int main(int argc, char* argv[])
     struct tucube_Core core;
     struct tucube_Core_Config coreConfig;
     struct tucube_Module_ConfigList moduleConfigList;
-    GON_C_LIST_INIT(&moduleConfigList);
+    GENC_LIST_INIT(&moduleConfigList);
     tucube_Options_process(argc, argv, &coreConfig, &moduleConfigList);
     tucube_Core_start(&core, &coreConfig, &moduleConfigList);
     json_decref(coreConfig.json);
-    GON_C_LIST_FOR_EACH(&moduleConfigList, struct tucube_Module_Config, moduleConfig)
+    GENC_LIST_FOR_EACH(&moduleConfigList, struct tucube_Module_Config, moduleConfig)
         json_decref(moduleConfig->json);
 
     return 0;
