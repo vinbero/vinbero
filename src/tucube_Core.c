e015a954d68ce858e3b994776fb7a8e5caf8e45d
Byeonggon Lee <gonny952@gmail.com>
2017-08-11 11:55:06 +0000

    Commit

--- src/tucube_Core.c
+++ src/tucube_Core.c
@@ -12,7 +12,6 @@
 #include <unistd.h>
 #include <libgenc/genc_cast.h>
 #include <libgenc/genc_list.h>
-#include <libgenc/genc_ltostr.h>
 #include "tucube_Core.h"
 
 static pthread_key_t tucube_Core_tlKey;
