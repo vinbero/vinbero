c94db9a29d3e4d2124988e42a284f327904b2da7
Byeonggon Lee <gonny952@gmail.com>
2017-11-13 16:58:30 +0900

    Commit

--- src/tucube.c
+++ src/tucube.c
@@ -6,6 +6,7 @@
 int main(int argc, char* argv[]) {
     struct tucube_Core core;
     struct tucube_Config config;
+    config.json = NULL;
     tucube_Options_process(argc, argv, &config);
     tucube_Core_start(&core, &config);
     json_decref(config.json);
