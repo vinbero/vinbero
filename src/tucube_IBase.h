6953894c45a9e7950c374e970565c63be3fd4ff4
Byeonggon Lee <gonny952@gmail.com>
2017-11-19 15:18:06 +0900

    Reduced jansson dependency

--- src/tucube_IBase.h
+++ src/tucube_IBase.h
@@ -9,16 +9,4 @@ int tucube_IBase_tlInit(struct tucube_Module* module, struct tucube_Config* conf
 int tucube_IBase_tlDestroy(struct tucube_Module* module);                                                                      \
 int tucube_IBase_destroy(struct tucube_Module* module)
 
-#define TUCUBE_IBASE_FUNCTION_POINTERS                                             \
-int (*tucube_IBase_init)(struct tucube_Config*, struct tucube_Module*, void*[]);   \
-int (*tucube_IBase_destroy)(struct tucube_Module*);                                \
-int (*tucube_IBase_tlInit)(struct tucube_Module*, struct tucube_Config*, void*[]); \
-int (*tucube_IBase_tlDestroy)(struct tucube_Module*)
-
-#define TUCUBE_IBASE_DLSYM(module, modulePointerType)                 \
-TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IBase_init);    \
-TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IBase_destroy); \
-TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IBase_tlInit);  \
-TUCUBE_MODULE_DLSYM(module, modulePointerType, tucube_IBase_tlDestroy)
-
 #endif
