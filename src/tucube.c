724ffd0ec2d23732263a68130a913e303fb2b8f2
Byeonggon Lee <gonny952@gmail.com>
2017-12-01 18:48:40 +0900

    Commit

--- src/tucube.c
+++ src/tucube.c
@@ -8,11 +8,13 @@ int main(int argc, char* argv[]) {
     GENC_TREE_NODE_INIT(&module);
     module.name = "core";
     module.localModule.pointer = malloc(1 * sizeof(struct tucube_Core));
+    module.interface = malloc(1 * sizeof(struct tucube_Core_Interface));
     struct tucube_Config config;
     config.json = NULL;
     tucube_Options_process(argc, argv, &config);
     tucube_Core_start(&module, &config);
     json_decref(config.json);
     free(module.localModule.pointer);
+    free(module.interface);
     return 0;
 }
--- src/tucube_Core.c
+++ src/tucube_Core.c
@@ -14,6 +14,7 @@
 #include <libgenc/genc_ArrayList.h>
 #include "tucube_Core.h"
 
+
 static pthread_key_t tucube_Core_tlKey;
 
 static void tucube_Core_sigIntHandler(int signal_number) {
@@ -47,27 +48,27 @@ static void tucube_Core_registerSignalHandlers() {
 
 static void tucube_Core_pthreadCleanupHandler(void* args) {
     struct tucube_Module* module = args;
-    struct tucube_Core* coreModule = module->localModule.pointer;
+    struct tucube_Core* localModule = module->localModule.pointer;
     GENC_TREE_NODE_FOR_EACH_CHILD(module, index) {
         struct tucube_Module* childModule = &GENC_TREE_NODE_GET_CHILD(module, index);
-        if(childModule->tucube_IBase_tlDestroy(childModule) == -1)
+        if(childModule->tucube_IModule_tlDestroy(childModule) == -1)
             warnx("%s: %u: tucube_Module_tlDestroy() failed", __FILE__, __LINE__);
     }
-    pthread_mutex_lock(coreModule->exitMutex);
-    coreModule->exit = true;
-    pthread_cond_signal(coreModule->exitCond);
-    pthread_mutex_unlock(coreModule->exitMutex);
+    pthread_mutex_lock(localModule->exitMutex);
+    localModule->exit = true;
+    pthread_cond_signal(localModule->exitCond);
+    pthread_mutex_unlock(localModule->exitMutex);
 }
 
 static void* tucube_Core_startWorker(void* args) {
     struct tucube_Module* module = ((void**)args)[0];
-    struct tucube_Core* coreModule = module->localModule.pointer;
+    struct tucube_Core* localModule = module->localModule.pointer;
     struct tucube_Config* config = ((void**)args)[1];
     pthread_cleanup_push(tucube_Core_pthreadCleanupHandler, module);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
     GENC_TREE_NODE_FOR_EACH_CHILD(module, index) {
         struct tucube_Module* childModule = &GENC_TREE_NODE_GET_CHILD(module, index);
-        if(childModule->tucube_IBase_tlInit(childModule, config, (void*[]){NULL}) == -1)
+        if(childModule->tucube_IModule_tlInit(childModule, config, (void*[]){NULL}) == -1)
             errx(EXIT_FAILURE, "%s: %u: tucube_Module_tlInit() failed", __FILE__, __LINE__);
     }
     sigset_t signalSet;
@@ -77,61 +78,57 @@ static void* tucube_Core_startWorker(void* args) {
         errx(EXIT_FAILURE, "%s: %u: pthread_sigmask() failed", __FILE__, __LINE__);
     GENC_TREE_NODE_FOR_EACH_CHILD(module, index) {
         struct tucube_Module* childModule = &GENC_TREE_NODE_GET_CHILD(module, index);
-        int (*pointer_tucube_ITlService_call)(struct tucube_Module*, void** args);
-        if((pointer_tucube_ITlService_call = dlsym(module->dlHandle, "tucube_ITlService_call")) == NULL)
+//        childModule->moduleInterface = malloc(1 * struct tucube_Core_Interface); // free() needed
+        if((childModule->moduleInterface->tucube_ITlService_call = dlsym(module->dlHandle, "tucube_ITlService_call")) == NULL)
             errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_ITlService_call()", __FILE__, __LINE__);
-        if(pointer_tucube_ITlService_call(childModule, (void*[]){&coreModule->serverSocket, NULL}) == -1)
+        if(childModule->moduleInterface->tucube_ITlService_call(childModule, (void*[]){&localModule->serverSocket, NULL}) == -1)
             errx(EXIT_FAILURE, "%s: %u: tucube_ITlService_call() failed", __FILE__, __LINE__);
     }
     pthread_cleanup_pop(1);
     return NULL;
 }
 
-struct tucube_Core_ChildModuleNames {
-    GENC_ARRAY_LIST(const char*);
-};
-
-static int tucube_Core_initCoreModule(struct tucube_Module* module, struct tucube_Config* config) {
-    struct tucube_Core* coreModule = module->localModule.pointer;
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.protocol", string, &coreModule->protocol, "TCP");
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.address", string, &coreModule->address, "0.0.0.0");
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.port", integer, &coreModule->port, 8080);
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.reusePort", integer, &coreModule->reusePort, 0);
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.backlog", integer, &coreModule->backlog, 1024);
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.workerCount", integer, &coreModule->workerCount, 4);
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.setUid", integer, &coreModule->setUid, geteuid());
-    TUCUBE_CONFIG_GET(config, module->name, "tucube.setGid", integer, &coreModule->setGid, getegid());
-    coreModule->exit = false;
-    coreModule->exitMutex = malloc(1 * sizeof(pthread_mutex_t));
-    pthread_mutex_init(coreModule->exitMutex, NULL);
-    coreModule->exitCond = malloc(1 * sizeof(pthread_cond_t));
-    pthread_cond_init(coreModule->exitCond, NULL);
+static int tucube_Core_initLocalModule(struct tucube_Module* module, struct tucube_Config* config) {
+    struct tucube_Core* localModule = module->localModule.pointer;
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.protocol", string, &localModule->protocol, "TCP");
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.address", string, &localModule->address, "0.0.0.0");
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.port", integer, &localModule->port, 8080);
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.reusePort", integer, &localModule->reusePort, 0);
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.backlog", integer, &localModule->backlog, 1024);
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.workerCount", integer, &localModule->workerCount, 4);
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.setUid", integer, &localModule->setUid, geteuid());
+    TUCUBE_CONFIG_GET(config, module->name, "tucube.setGid", integer, &localModule->setGid, getegid());
+    localModule->exit = false;
+    localModule->exitMutex = malloc(1 * sizeof(pthread_mutex_t));
+    pthread_mutex_init(localModule->exitMutex, NULL);
+    localModule->exitCond = malloc(1 * sizeof(pthread_cond_t));
+    pthread_cond_init(localModule->exitCond, NULL);
 
     struct sockaddr_in serverAddressSockAddrIn;
     memset(serverAddressSockAddrIn.sin_zero, 0, 1 * sizeof(serverAddressSockAddrIn.sin_zero));
     serverAddressSockAddrIn.sin_family = AF_INET; 
 
-    inet_aton(coreModule->address, &serverAddressSockAddrIn.sin_addr);
-    serverAddressSockAddrIn.sin_port = htons(coreModule->port);
+    inet_aton(localModule->address, &serverAddressSockAddrIn.sin_addr);
+    serverAddressSockAddrIn.sin_port = htons(localModule->port);
 
-    if(strcmp(coreModule->protocol, "TCP") == 0) {
-        if((coreModule->serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
+    if(strcmp(localModule->protocol, "TCP") == 0) {
+        if((localModule->serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
             err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
-        if(setsockopt(coreModule->serverSocket, SOL_SOCKET, SO_REUSEADDR, &(const int){1}, sizeof(int)) == -1)
+        if(setsockopt(localModule->serverSocket, SOL_SOCKET, SO_REUSEADDR, &(const int){1}, sizeof(int)) == -1)
             err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
-        if(setsockopt(coreModule->serverSocket, SOL_SOCKET, SO_REUSEPORT, &coreModule->reusePort, sizeof(int)) == -1)
+        if(setsockopt(localModule->serverSocket, SOL_SOCKET, SO_REUSEPORT, &localModule->reusePort, sizeof(int)) == -1)
             warn("%s: %u", __FILE__, __LINE__);
-        if(bind(coreModule->serverSocket, (struct sockaddr*)&serverAddressSockAddrIn, sizeof(struct sockaddr)) == -1)
+        if(bind(localModule->serverSocket, (struct sockaddr*)&serverAddressSockAddrIn, sizeof(struct sockaddr)) == -1)
             err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
-        if(listen(coreModule->serverSocket, coreModule->backlog) == -1)
+        if(listen(localModule->serverSocket, localModule->backlog) == -1)
             err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
-    } else if(strcmp(coreModule->protocol, "UDP") == 0) {
-        if((coreModule->serverSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    } else if(strcmp(localModule->protocol, "UDP") == 0) {
+        if((localModule->serverSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
             err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
-        if(bind(coreModule->serverSocket, (struct sockaddr*)&serverAddressSockAddrIn, sizeof(struct sockaddr)) == -1)
+        if(bind(localModule->serverSocket, (struct sockaddr*)&serverAddressSockAddrIn, sizeof(struct sockaddr)) == -1)
             err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
     } else
-        errx(EXIT_FAILURE, "%s: %u: Unknown protocol %s", __FILE__, __LINE__, coreModule->protocol);
+        errx(EXIT_FAILURE, "%s: %u: Unknown protocol %s", __FILE__, __LINE__, localModule->protocol);
 
     return 0;
 }
@@ -150,15 +147,15 @@ static int tucube_Core_initChildModules(struct tucube_Module* module, struct tuc
         TUCUBE_CONFIG_GET_MODULE_PATH(config, childModule->name, &childModulePath);
         if((childModule->dlHandle = dlopen(childModulePath, RTLD_LAZY | RTLD_GLOBAL)) == NULL)
             errx(EXIT_FAILURE, "%s: %u: dlopen() failed, possible causes are:\n1. Unable to find next module\n2. The next module didn't linked required shared libraries properly", __FILE__, __LINE__);
-        if((childModule->tucube_IBase_init = dlsym(module->dlHandle, "tucube_IBase_init")) == NULL)
-            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IBase_init()", __FILE__, __LINE__);
-        if((childModule->tucube_IBase_tlInit = dlsym(module->dlHandle, "tucube_IBase_tlInit")) == NULL)
-            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IBase_tlInit()", __FILE__, __LINE__);
-        if((childModule->tucube_IBase_tlDestroy = dlsym(module->dlHandle, "tucube_IBase_tlDestroy")) == NULL)
-            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IBase_tlDestroy()", __FILE__, __LINE__);
-        if((childModule->tucube_IBase_destroy = dlsym(module->dlHandle, "tucube_IBase_destroy")) == NULL)
-            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IBase_destroy()", __FILE__, __LINE__);
-        childModule->tucube_IBase_init(childModule, config, (void*[]){NULL});
+        if((childModule->tucube_IModule_init = dlsym(module->dlHandle, "tucube_IModule_init")) == NULL)
+            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IModule_init()", __FILE__, __LINE__);
+        if((childModule->tucube_IModule_tlInit = dlsym(module->dlHandle, "tucube_IModule_tlInit")) == NULL)
+            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IModule_tlInit()", __FILE__, __LINE__);
+        if((childModule->tucube_IModule_tlDestroy = dlsym(module->dlHandle, "tucube_IModule_tlDestroy")) == NULL)
+            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IModule_tlDestroy()", __FILE__, __LINE__);
+        if((childModule->tucube_IModule_destroy = dlsym(module->dlHandle, "tucube_IModule_destroy")) == NULL)
+            errx(EXIT_FAILURE, "%s: %u: Unable to find tucube_IModule_destroy()", __FILE__, __LINE__);
+        childModule->tucube_IModule_init(childModule, config, (void*[]){NULL});
         tucube_Core_initChildModules(childModule, config);
     }
     GENC_ARRAY_LIST_FREE(&childModuleNames);
@@ -168,7 +165,7 @@ static int tucube_Core_initChildModules(struct tucube_Module* module, struct tuc
 static int tucube_Core_destroyChildModules(struct tucube_Module* module) {
     GENC_TREE_NODE_FOR_EACH_CHILD(module, index) {
         struct tucube_Module* childModule = &GENC_TREE_NODE_GET_CHILD(module, index);
-        if(childModule->tucube_IBase_destroy(childModule) == -1)
+        if(childModule->tucube_IModule_destroy(childModule) == -1)
            warn("%s: %u", __FILE__, __LINE__);
         tucube_Core_destroyChildModules(childModule);
     }
@@ -176,18 +173,18 @@ static int tucube_Core_destroyChildModules(struct tucube_Module* module) {
 }
 
 static int tucube_Core_init(struct tucube_Module* module, struct tucube_Config* config) {
-    struct tucube_Core* coreModule = module->localModule.pointer;
-    tucube_Core_initCoreModule(module, config);
+    struct tucube_Core* localModule = module->localModule.pointer;
+    tucube_Core_initLocalModule(module, config);
     tucube_Core_initChildModules(module, config);
-    if(setgid(coreModule->setGid) == -1)
+    if(setgid(localModule->setGid) == -1)
         err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
-    if(setuid(coreModule->setUid) == -1)
+    if(setuid(localModule->setUid) == -1)
         err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
     return 0;
 }
 
 int tucube_Core_start(struct tucube_Module* module, struct tucube_Config* config) {
-    struct tucube_Core* coreModule = module->localModule.pointer;
+    struct tucube_Core* localModule = module->localModule.pointer;
     tucube_Core_init(module, config);
     tucube_Core_registerSignalHandlers();
     pthread_t* workerThreads;
@@ -200,51 +197,51 @@ int tucube_Core_start(struct tucube_Module* module, struct tucube_Config* config
         pthread_attr_init(&coreThreadAttr);
         pthread_attr_setdetachstate(&coreThreadAttr, PTHREAD_CREATE_JOINABLE);
 
-        workerThreads = malloc(coreModule->workerCount * sizeof(pthread_t));
+        workerThreads = malloc(localModule->workerCount * sizeof(pthread_t));
 
         atexit(tucube_Core_exitHandler);
 
-        for(size_t index = 0; index != coreModule->workerCount; ++index) {
+        for(size_t index = 0; index != localModule->workerCount; ++index) {
            if(pthread_create(workerThreads + index, &coreThreadAttr, tucube_Core_startWorker, (void*[]){module, config}) != 0)
                 err(EXIT_FAILURE, "%s: %u", __FILE__, __LINE__);
         }
 
-        pthread_mutex_lock(coreModule->exitMutex);
-        while(coreModule->exit != true) {
-            pthread_cond_wait(coreModule->exitCond,
-                 coreModule->exitMutex);
+        pthread_mutex_lock(localModule->exitMutex);
+        while(localModule->exit != true) {
+            pthread_cond_wait(localModule->exitCond,
+                 localModule->exitMutex);
         }
-        pthread_mutex_unlock(coreModule->exitMutex);
+        pthread_mutex_unlock(localModule->exitMutex);
 
-        for(size_t index = 0; index != coreModule->workerCount; ++index) {
+        for(size_t index = 0; index != localModule->workerCount; ++index) {
             pthread_cancel(workerThreads[index]);
             pthread_join(workerThreads[index], NULL);
         }
-        coreModule->exit = true;
+        localModule->exit = true;
     }
     free(jumpBuffer);
     pthread_key_delete(tucube_Core_tlKey);
-    pthread_mutex_unlock(coreModule->exitMutex);
+    pthread_mutex_unlock(localModule->exitMutex);
 
-    if(coreModule->exit == false) {
-        for(size_t index = 0; index != coreModule->workerCount; ++index) {
+    if(localModule->exit == false) {
+        for(size_t index = 0; index != localModule->workerCount; ++index) {
             pthread_cancel(workerThreads[index]);
-            pthread_mutex_lock(coreModule->exitMutex);
-            while(coreModule->exit != true)
-                pthread_cond_wait(coreModule->exitCond, coreModule->exitMutex);
-            pthread_mutex_unlock(coreModule->exitMutex);
+            pthread_mutex_lock(localModule->exitMutex);
+            while(localModule->exit != true)
+                pthread_cond_wait(localModule->exitCond, localModule->exitMutex);
+            pthread_mutex_unlock(localModule->exitMutex);
             pthread_join(workerThreads[index], NULL);
-            coreModule->exit = false;
+            localModule->exit = false;
         }
     }
 
-    pthread_cond_destroy(coreModule->exitCond);
-    free(coreModule->exitCond);
-    pthread_mutex_destroy(coreModule->exitMutex);
-    free(coreModule->exitMutex);
-    close(coreModule->serverSocket);
+    pthread_cond_destroy(localModule->exitCond);
+    free(localModule->exitCond);
+    pthread_mutex_destroy(localModule->exitMutex);
+    free(localModule->exitMutex);
+    close(localModule->serverSocket);
     pthread_attr_destroy(&coreThreadAttr);
     free(workerThreads);
-//    dlclose(coreModule->dlHandle);
+//    dlclose(localModule->dlHandle);
     return 0;
 }
