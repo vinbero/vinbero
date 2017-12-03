5e31b855d4b95f59f31bdff49ccbb3f3aa8bebaa
Byeonggon Lee <gonny952@gmail.com>
2017-12-03 12:15:36 +0900

    Fix bugs

--- src/tucube_IModule.h
+++ src/tucube_IModule.h
@@ -4,10 +4,10 @@
 #include "tucube_Module.h"
 
 #define TUCUBE_IMODULE_FUNCTIONS                                                                     \
-int tucube_IModule_init(struct tucube_Config* config, void* args[]);                                 \
+int tucube_IModule_init(struct tucube_Module* module, struct tucube_Config* config, void* args[]);   \
+int tucube_IModule_destroy(struct tucube_Module* module);                                            \
 int tucube_IModule_tlInit(struct tucube_Module* module, struct tucube_Config* config, void* args[]); \
-int tucube_IModule_tlDestroy(struct tucube_Module* module);                                          \
-int tucube_IModule_destroy(struct tucube_Module* module)
+int tucube_IModule_tlDestroy(struct tucube_Module* module)
 
 #define TUCUBE_IMODULE_FUNCTION_POINTERS                                             \
 int (*tucube_IModule_init)(struct tucube_Module*, struct tucube_Config*, void*[]);   \
