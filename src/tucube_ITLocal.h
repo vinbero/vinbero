ef194827b26e5446c99fd50d5a04ec4bc0539409
Byeonggon Lee <gonny952@gmail.com>
2017-12-28 19:47:31 +0900

    Commit

--- src/tucube_ITLocal.h
+++ src/tucube_ITLocal.h
@@ -3,12 +3,16 @@
 
 #include "tucube_Module.h"
 
-#define TUCUBE_ITLOCAL_FUNCTIONS                                                                   \
-int tucube_ITLocal_init(struct tucube_Module* module, struct tucube_Config* config, void* args[]); \
-int tucube_ITLocal_destroy(struct tucube_Module* module)
+#define TUCUBE_ITLOCAL_FUNCTIONS                                                                    \
+int tucube_ITLocal_init(struct tucube_Module* module, struct tucube_Config* config, void* args[]);  \
+int tucube_ITLocal_rInit(struct tucube_Module* module, struct tucube_Config* config, void* args[]); \
+int tucube_ITLocal_destroy(struct tucube_Module* module);                                           \
+int tucube_ITLocal_rDestroy(struct tucube_Module* module)
 
-#define TUCUBE_ITLOCAL_FUNCTION_POINTERS                                           \
-int (*tucube_ITLocal_init)(struct tucube_Module*, struct tucube_Config*, void*[]); \
-int (*tucube_ITLocal_destroy)(struct tucube_Module*)
+#define TUCUBE_ITLOCAL_FUNCTION_POINTERS                                            \
+int (*tucube_ITLocal_init)(struct tucube_Module*, struct tucube_Config*, void*[]);  \
+int (*tucube_ITLocal_rInit)(struct tucube_Module*, struct tucube_Config*, void*[]); \
+int (*tucube_ITLocal_destroy)(struct tucube_Module*);                               \
+int (*tucube_ITLocal_rDestroy)(struct tucube_Module*)
 
 #endif
