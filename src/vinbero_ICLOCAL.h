237e9c7a190b8d93b571914a6d1c15a120fb44a5
Byeonggon Lee <gonny952@gmail.com>
2018-05-31 11:48:16 +0900

    commit

--- src/vinbero_ICLOCAL.h
+++ src/vinbero_ICLOCAL.h
@@ -2,14 +2,15 @@
 #define _VINBERO_ICLOCAL_H
 
 #include <vinbero_common/vinbero_common_Module.h>
+#include <vinbero_common/vinbero_common_ClData.h>
 
 #define VINBERO_ICLOCAL_FUNCTIONS \
-int vinbero_ICLOCAL_init(struct vinbero_common_Module* module, struct vinbero_ClData* clData, void* args[]); \
-int vinbero_ICLOCAL_destroy(struct vinbero_common_Module* module, struct vinbero_ClData* clData)
+int vinbero_ICLOCAL_init(struct vinbero_common_Module* module, struct vinbero_common_ClData* clData, void* args[]); \
+int vinbero_ICLOCAL_destroy(struct vinbero_common_Module* module, struct vinbero_common_ClData* clData)
 
 #define VINBERO_ICLOCAL_FUNCTION_POINTERS \
-int (*vinbero_ICLOCAL_init)(struct vinbero_common_Module*, struct vinbero_ClData*, void*[]); \
-int (*vinbero_ICLOCAL_destroy)(struct vinbero_common_Module*, struct vinbero_ClData*)
+int (*vinbero_ICLOCAL_init)(struct vinbero_common_Module*, struct vinbero_common_ClData*, void*[]); \
+int (*vinbero_ICLOCAL_destroy)(struct vinbero_common_Module*, struct vinbero_common_ClData*)
 
 struct vinbero_ICLOCAL_Interface {
     VINBERO_ICLOCAL_FUNCTION_POINTERS;
