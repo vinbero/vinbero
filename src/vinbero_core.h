b83da454e0ae18317a90a0434717856e64beb4cd
Byeonggon Lee <gonny952@gmail.com>
2018-06-02 18:55:37 +0900

    Commit

--- src/vinbero_core.h
+++ src/vinbero_core.h
@@ -8,14 +8,12 @@ int vinbero_core_registerSignalHandlers();
 void vinbero_core_registerExitHandler();
 int vinbero_core_checkConfig(struct vinbero_common_Config* config, const char* moduleId);
 int vinbero_core_initLocalModule(struct vinbero_common_Module* module, struct vinbero_common_Config* config);
-int vinbero_core_start(struct vinbero_common_Module* module, struct vinbero_common_Config* config);
-int vinbero_core_loadChildModules(struct vinbero_common_Module* module, struct vinbero_common_Module* parentModule, const char* moduleId, struct vinbero_common_Config* config);
-int vinbero_core_initChildModules(struct vinbero_common_Module* module, struct vinbero_common_Config* config);
-int vinbero_core_rInitChildModules(struct vinbero_common_Module* module, struct vinbero_common_Config* config);
-int vinbero_core_destroyChildModules(struct vinbero_common_Module* module);
-int vinbero_core_rDestroyChildModules(struct vinbero_common_Module* module);
+int vinbero_core_loadChildModules(struct vinbero_common_Module* module);
+int vinbero_core_initChildModules(struct vinbero_common_Module* module);
+int vinbero_core_rInitChildModules(struct vinbero_common_Module* module);
+int vinbero_core_sendArgsChildModules(struct vinbero_common_Module* module);
+int vinbero_core_start(struct vinbero_common_Module* module);
 int vinbero_core_setGid(struct vinbero_common_Module* module);
 int vinbero_core_setUid(struct vinbero_common_Module* module);
-
 
 #endif
