f3e7fa7899f76ed1bfde29739fa89c5df9568b48
Byeonggon Lee <gonny952@gmail.com>
2018-06-07 20:41:26 +0900

    Commit

--- test/vinbero_Options_test.c
+++ test/vinbero_Options_test.c
@@ -13,6 +13,10 @@ void test_vinbero_Options_process(void** state) {
     vinbero_Options_process(2, args, NULL);
 }
 
+void __wrap_vinbero_common_Log_init(int flag) {}
+void __wrap_vinbero_common_Log_raw(int level, const char* source, int line, const char* format, ...) {}
+void __wrap_vinbero_Help_printAndExit() {}
+
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_vinbero_Options_process)
