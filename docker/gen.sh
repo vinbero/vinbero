3865f09d57ca2bbc66908ce69400e3813af0351e
Byeonggon Lee <gonny952@gmail.com>
2019-02-19 09:38:29 +0900

    Remove unused files

--- docker/gen.sh
+++
@@ -1,8 +0,0 @@
-echo $0
-for dirpath in $(ls -d $(dirname $0)/*/ )
-do
-  echo $dirpath
-  dir=$(basename $dirpath)
-  echo directory $dir "{\"branch\":\"$dir\"}"
-  echo "{\"branch\":\"$dir\"}" | gmcr - Dockerfile.gmcr > $dirpath/Dockerfile
-done
